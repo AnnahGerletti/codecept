8773952d21d24f7b68677e05ed93f8ac29de66e0
AnnahGerletti <agerletti@gmail.com>
2017-12-05 14:10:12 +1300

    created new test for returning user

--- test/init_variables_test.js
+++ test/init_variables_test.js
@@ -2,10 +2,14 @@
 
 Feature('site knows users pressence');
 
+//
 Scenario('new user 1st time to site', (I) => {
   I.amOnPage("http://tapin.nz/tapinknow")
   I.see('Hello First time user')
 });
-// would need a cookie or not have a cookie
 
-// Scenario('')
+//eventually will need to add cookie permissions, to be able to test what a returning user looks like.
+Scenario('return user', (I) => {
+  I.amOnPage("http://tapin.nz/tapinknow")
+  I.see('Wellcome Back')
+})
