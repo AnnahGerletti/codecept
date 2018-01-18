4c91f6b48cd53d33eb763779eefa0edb34ea92e9
AnnahGerletti <agerletti@gmail.com>
2018-01-18 13:33:07 +1300

    creating scenaro test for login pg and main page

--- test/login_page_test.js
+++ test/login_page_test.js
@@ -1,7 +1,17 @@
 
 Feature('login page')
 
-Scenario('see login page', (I) => {
+Sceanario('see login page', (I) => {
+  I.amOnPage('/login')
+  I.seeElement('#login')
+})
+
+Scenario('see tapin know logo', (I) => {
+  I
+})
+// figure out how to see a logo image that would be in the css headers probably 
+
+Scenario('can login', (I) => {
   const userName = 'johnsmith'
   const password = 'password'
   I.amOnPage('/login')
